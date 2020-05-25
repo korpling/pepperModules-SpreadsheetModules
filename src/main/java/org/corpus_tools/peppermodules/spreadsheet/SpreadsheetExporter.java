4cf19ead844f74fcee9a9015980ffdaafe7462ac
Martin Klotz <MartinKl@hu-berlin.de>
2020-05-25 16:29:38 +0200

    create mapper

--- src/main/java/org/corpus_tools/peppermodules/spreadsheet/SpreadsheetExporter.java
+++ src/main/java/org/corpus_tools/peppermodules/spreadsheet/SpreadsheetExporter.java
@@ -2,9 +2,21 @@ package org.corpus_tools.peppermodules.spreadsheet;
 
 import org.corpus_tools.pepper.impl.PepperExporterImpl;
 import org.corpus_tools.pepper.modules.PepperExporter;
+import org.corpus_tools.pepper.modules.PepperMapper;
+import org.corpus_tools.salt.common.SDocument;
+import org.corpus_tools.salt.graph.Identifier;
+import org.eclipse.emf.common.util.URI;
 import org.osgi.service.component.annotations.Component;
 
 @Component(name="SpreadsheetExporterComponent", factory="PepperExporterComponentFactory")
 public class SpreadsheetExporter extends PepperExporterImpl implements PepperExporter {
-
+	@Override
+	public PepperMapper createPepperMapper(Identifier identifier) {
+		Salt2SpreadsheetMapper mapper = new Salt2SpreadsheetMapper();
+		if (identifier.getIdentifiableElement() != null && identifier.getIdentifiableElement() instanceof SDocument) {
+			URI resource = getIdentifier2ResourceTable().get(identifier);
+			mapper.setResourceURI(resource);
+		}
+		return mapper;
+	}
 }
