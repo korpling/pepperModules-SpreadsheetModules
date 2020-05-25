9c464e83c831ed7e047927fc6bc8ee1c0dcf953b
Martin Klotz <MartinKl@hu-berlin.de>
2020-05-25 16:29:32 +0200

    created mapSDocument()

--- src/main/java/org/corpus_tools/peppermodules/spreadsheet/Salt2SpreadsheetMapper.java
+++ src/main/java/org/corpus_tools/peppermodules/spreadsheet/Salt2SpreadsheetMapper.java
@@ -1,8 +1,12 @@
 package org.corpus_tools.peppermodules.spreadsheet;
 
+import org.corpus_tools.pepper.common.DOCUMENT_STATUS;
 import org.corpus_tools.pepper.impl.PepperMapperImpl;
 import org.corpus_tools.pepper.modules.PepperMapper;
 
 public class Salt2SpreadsheetMapper extends PepperMapperImpl implements PepperMapper {
-
+	@Override
+	public DOCUMENT_STATUS mapSDocument() {
+		return DOCUMENT_STATUS.COMPLETED;
+	}
 }
