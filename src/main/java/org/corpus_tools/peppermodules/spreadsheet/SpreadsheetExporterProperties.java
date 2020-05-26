d1d1151f44106e9df573e1a97c49505117237ab0
Martin Klotz <MartinKl@hu-berlin.de>
2020-05-26 19:24:37 +0200

    added column order property

--- src/main/java/org/corpus_tools/peppermodules/spreadsheet/SpreadsheetExporterProperties.java
+++ src/main/java/org/corpus_tools/peppermodules/spreadsheet/SpreadsheetExporterProperties.java
@@ -1,11 +1,17 @@
 package org.corpus_tools.peppermodules.spreadsheet;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.corpus_tools.pepper.modules.PepperModuleProperties;
 import org.corpus_tools.pepper.modules.PepperModuleProperty;
 
 public class SpreadsheetExporterProperties extends PepperModuleProperties {
 	/** Sets the font is to be used in the spreadsheet table (this influences the covered unicode characters). */
 	private static final String PROP_FONT_NAME = "font.name";
+	/** Configure the order of columns as comma-separated annotation names (qualified). Partial configurations are allowed. If no configuration is provided for an annotation, the next free column is used. */
+	private static final String PROP_COL_ORDER = "column.order";
 	
 	public SpreadsheetExporterProperties() {
 		addProperty(PepperModuleProperty.create()
@@ -13,6 +19,12 @@ public class SpreadsheetExporterProperties extends PepperModuleProperties {
 				.withType(String.class)
 				.withDescription("Sets the font is to be used in the spreadsheet table (this influences the covered unicode characters).")
 				.withDefaultValue("DejaVu Sans").build());
+		addProperty(PepperModuleProperty.create()
+				.withName(PROP_COL_ORDER)
+				.withType(String.class)
+				.withDescription("Configure the order of columns as comma-separated annotation names (qualified). Partial configurations are allowed. If no configuration is provided for an annotation, the next free column is used.")
+				.isRequired(false)
+				.build());
 	}
 	
 	/**
@@ -21,5 +33,17 @@ public class SpreadsheetExporterProperties extends PepperModuleProperties {
 	 */
 	public String getFont() {
 		return (String) getProperty(PROP_FONT_NAME).getValue();
+	}
+	
+	public Map<String, Integer> getColumnOrder() {
+		Object val = getProperty(PROP_COL_ORDER).getValue();
+		if (val == null) {
+			return null;
+		}
+		Map<String, Integer> columnOrder = new HashMap<>();
+		for (String qName : StringUtils.split((String) val)) {
+			columnOrder.put(qName, columnOrder.values().size());
+		}
+		return columnOrder;
 	}
 }
