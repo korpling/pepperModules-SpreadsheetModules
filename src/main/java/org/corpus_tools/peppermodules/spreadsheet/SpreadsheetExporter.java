ec203b9abe3516a396a5c5b9379c69a922c887de
Martin Klotz <MartinKl@hu-berlin.de>
2020-06-02 12:45:02 +0200

    initial module config in constructor was missing

--- src/main/java/org/corpus_tools/peppermodules/spreadsheet/SpreadsheetExporter.java
+++ src/main/java/org/corpus_tools/peppermodules/spreadsheet/SpreadsheetExporter.java
@@ -1,5 +1,6 @@
 package org.corpus_tools.peppermodules.spreadsheet;
 
+import org.corpus_tools.pepper.common.PepperConfiguration;
 import org.corpus_tools.pepper.impl.PepperExporterImpl;
 import org.corpus_tools.pepper.modules.PepperExporter;
 import org.corpus_tools.pepper.modules.PepperMapper;
@@ -11,8 +12,14 @@ import org.osgi.service.component.annotations.Component;
 @Component(name="SpreadsheetExporterComponent", factory="PepperExporterComponentFactory")
 public class SpreadsheetExporter extends PepperExporterImpl implements PepperExporter {
 	
+	private static final String NAME = "SpreadsheetExporter";
+
 	public SpreadsheetExporter() {
-		setProperties(new SpreadsheetExporterProperties());
+		setSupplierContact(URI.createFileURI(PepperConfiguration.EMAIL));
+		setName(NAME);
+		addSupportedFormat("xlsx", "2007+", null);
+		setIsMultithreaded(true);
+		setProperties(new SpreadsheetExporterProperties());		
 	}
 	
 	@Override
